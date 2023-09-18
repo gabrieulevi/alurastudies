8f9e58a91a0ac635552bbb84e8324a11ab1b5d8b
gabriel <gabriel@newnet.com.br>
2023-09-17 23:21:30 -0300

    first POST http method applied using express

--- apiexpress/src/app.js
+++ apiexpress/src/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json())
 const books = [
   {
     id: 1,
@@ -10,15 +10,21 @@ const books = [
   {
     id: 2,
     title: "A morte de Ivan Ilitch",
-  },
+  }
 ];
 
 app.get("/", (req, res) => {
   res.status(200).send("Hello world (home)");
-  console.log(req.url, res);
+  console.log(req.url, res.status);
 });
 
 app.get("/books", (req, res) => {
   res.status(200).json(books);
 });
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  console.log(req.body)
+  res.status(201).send("Book successfully created")
+})
 export default app;
