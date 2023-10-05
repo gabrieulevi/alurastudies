ed25e6eb8fd18da5e1163ea53b73aec2b1e1f836
gabrieulevi <gabriel@newnet.com.br>
2023-10-05 16:33:50 -0300

    CRUD completed

--- orm/api/controllers/PessoasController.js
+++ orm/api/controllers/PessoasController.js
@@ -7,16 +7,63 @@ class PessoasController{
             return res.status(200).json(todasAsPessoas);
         } catch (error) {
             console.log(error);
+            res.status(500).send(error);
         }
     }
     static async pegaUmaPessoa(req, res){
-        const {id} = req.params
+        const { id } = req.params
         try {
             console.log(id);
             const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } });
-            res.status(200).json({umaPessoa});
+            res.status(200).json(umaPessoa);
         } catch (error) {
             console.log(error);
+            res.status(500).send(error);
+        }
+    }
+    static async criaPessoa(req, res){
+        const novaPessoa = req.body;
+
+        try {
+            console.log(novaPessoa);
+            const novaPessoaCriada = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(novaPessoaCriada);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+    static async atualizaPessoa(req, res){
+        const novosDados = req.body;
+        const {id} = req.params
+        try {
+            await database.Pessoas.update(novosDados, {
+                where: {
+                    id: Number(id)
+                }
+            })
+            const pessoaAtualizada = await database.Pessoas.findOne({
+                where: {
+                    id: id
+                }
+            })
+            res.status(200).json(pessoaAtualizada);
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+    static async removePessoa(req, res){
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({
+                where: {
+                    id: Number(id)
+                }
+            })
+        } catch (error) {
+            console.log(error);
+            res.status(500).send(error)
         }
     }
 }
--- orm/api/routes/pessoasRouter.js
+++ orm/api/routes/pessoasRouter.js
@@ -5,5 +5,8 @@ const router = Router();
 
 router.get('/pessoas', PessoasController.pegaTodasAsPessoas);
 router.get('/pessoas/:id', PessoasController.pegaUmaPessoa);
+router.post('/pessoas', PessoasController.criaPessoa);
+router.put('/pessoas/:id', PessoasController.atualizaPessoa);
+router.delete('/pessoas/:id', PessoasController.removePessoa);
 
 module.exports = router;
