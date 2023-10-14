d21c4e22d79c86b7c569d3c14129d1a3ce9db1ec
gabrieulevi <gabriel@newnet.com.br>
2023-10-14 19:58:38 -0300

    socket io connection working

--- websocket/src/servidor.js
+++ websocket/src/servidor.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import path from 'path';
 import url from 'url';
-
+import http from 'http'
+import { Server } from 'socket.io';
 const porta = process.env.porta || 3000;
 
+const app = express();
 const caminhoAtual = url.fileURLToPath(import.meta.url);
 const diretorioPublico  = path.join(caminhoAtual, "../..", "public");
 
-const app = express();
-
-app.listen(porta, () => console.log(`o servidor subiu e está escutando na porta ${porta}`));
 app.use(express.static(diretorioPublico));
+
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(porta, () => console.log(`o servidor subiu e está escutando na porta ${porta}`));
+
+const io = new Server(servidorHttp);
+
+io.on('connection', ()=> console.log("conexão realizada"))
