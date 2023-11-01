d11adc234de07cb2cd27f08d3b8f21e6c644441f
gabrieulevi <gabriel@newnet.com.br>
2023-11-01 14:42:28 -0300

    iniciando projeto com next js

---
+++ alura-next/src/components/Link/index.js
@@ -0,0 +1,9 @@
+import link from "next/link"
+
+export default function Link({children, href, ...props}){
+    return (
+        <NextLink href={href} passHref>
+            <a {...props}>{children}</a>
+        </NextLink>
+    )
+}
