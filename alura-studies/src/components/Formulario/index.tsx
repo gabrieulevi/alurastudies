7d6065e90b080b25610a67798750938ccf17ecd1
gabriel <gabriel@newnet.com.br>
2023-11-04 19:28:00 -0300

    usando props

--- alura-studies/src/components/Formulario/index.tsx
+++ alura-studies/src/components/Formulario/index.tsx
@@ -30,7 +30,9 @@ class Formulario extends React.Component {
                     required
                     />
                 </div>
-                <Botao/>
+                <Botao>
+                 Adicionar
+                </Botao>
             </form>
         )
     }
